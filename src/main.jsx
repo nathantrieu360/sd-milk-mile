845e164d77fca5f3e92e0473040dc7a72fa327e3
nathantrieu360 <nathantrieu360@gmail.com>
2026-05-23 23:34:18 -0700

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import SDMilkMile from './SDMilkMile'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <SDMilkMile />
+  </StrictMode>
+)
